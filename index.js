c2ea27027d086c9af2102b803ff347ecda3536de
russellpbrown@gmail.com <russellpbrown@gmail.com>
2018-06-20 20:23:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
   delete newObject.key
+  return newObject
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
