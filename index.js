785ae24cde5ecd97023244731bb32afc7123b357
russellpbrown@gmail.com <russellpbrown@gmail.com>
2018-06-20 20:32:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = Object.assign({}, object)
   newObject[key] = value
+  return newObject
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
